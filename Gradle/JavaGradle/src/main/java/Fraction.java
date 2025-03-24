06f137b98f347eb0a7293dca7af7768ce4b6010d
Nicholas Nastase <nastasenick22@gmail.com>
2025-03-24 17:33:31 -0400

    Opened a bunch of files. Edit JavaGradle Fraction.java file to accept 2 args.

--- Gradle/JavaGradle/src/main/java/Fraction.java
+++ Gradle/JavaGradle/src/main/java/Fraction.java
@@ -9,13 +9,15 @@ import java.io.*;
  *         Software Engineering, CIDSE, IAFSE, ASU Poly
  * @version January 2020
  */
+
+//nnastase 3/24/2025 updating to take 0-2 inputs
 public class Fraction {
 
    private int numerator, denominator;
 
    public Fraction(){
-      numerator = denominator = 0;
-   }
+      numerator =1; denominator = 1;
+   }  //change for default
 
    public void print() {
     System.out.print(numerator + "/" + denominator );
@@ -26,7 +28,13 @@ public class Fraction {
    }
 
    public void setDenominator (int d) {
+      //make sure non-zero
+      if (d==0){
+         System.out.println("denom cannot be 0.");
+         System.exit(1);      //exit if 0
+      }
       denominator = d;
+
    }
 
    public int getDenominator() {
@@ -44,14 +52,20 @@ public class Fraction {
          Fraction frac = new Fraction();
 
          // set the values
-         frac.setNumerator(1);
-         frac.setDenominator(3);
+         if (args.length > 0) {
+            frac.setNumerator(Integer.parseInt(args[0]));
+         }
+         if (args.length > 1) {
+            frac.setDenominator(Integer.parseInt(args[1]));
+         }
 
          // print it
          System.out.print("The fraction is: ");
          frac.print();
          System.out.println("");
 
+      }catch (NumberFormatException e){
+         System.out.println("error, provide proper int values")
       }catch(Exception e) {
          e.printStackTrace();
       }
